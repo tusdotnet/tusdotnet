cdd0c617796b476c0a4d3133b1f68af974ba7501
Stefan Matsson <s.matsson@gmail.com>
2018-12-07 11:33:24 +0100

    Add test site for ASP.NET Core 2.2 on .NET Core 2.2
    Tested using both inprocess and outofprocess hosting models and both using IIS and running as a console app

---
+++ Source/TestSites/AspNetCore_netcoreapp2.2_TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCore_netcoreapp2_2_TestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureKestrel(options =>
+                {
+                    options.Limits.MaxRequestBodySize = null;
+                    options.Limits.MaxRequestBufferSize = null;
+                });
+    }
+}
