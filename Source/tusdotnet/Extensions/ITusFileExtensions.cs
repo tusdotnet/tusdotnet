43155b5942c9f9f52a36be2acfe66a2c76aa2963
Stefan Matsson <s.matsson@gmail.com>
2019-01-21 15:00:50 +0100

    Add helper for checking if a ITusFile is completely uploaded

---
+++ Source/tusdotnet/Extensions/ITusFileExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using tusdotnet.Interfaces;
+
+namespace tusdotnet.Extensions
+{
+    /// <summary>
+    /// Extension methods for <see cref="ITusFile"/>
+    /// </summary>
+    public static class ITusFileExtensions
+    {
+        /// <summary>
+        /// Check if the <see cref="ITusFile"/> has been completely uploaded.
+        /// </summary>
+        /// <param name="file">The file to check</param>
+        /// <param name="store">The store responsible for handling the file</param>
+        /// <param name="cancellationToken">The cancellation token to use when cancelling</param>
+        /// <returns>True if the file is completed, otherwise false</returns>
+        public static async Task<bool> IsCompleteAsync(this ITusFile file, ITusStore store, CancellationToken cancellationToken)
+        {
+            var length = store.GetUploadLengthAsync(file.Id, cancellationToken);
+            var offset = store.GetUploadOffsetAsync(file.Id, cancellationToken);
+
+            await Task.WhenAll(length, offset);
+
+            return length.Result == offset.Result;
+        }
+    }
+}
