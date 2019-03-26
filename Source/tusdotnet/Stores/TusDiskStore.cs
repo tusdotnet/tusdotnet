3454afb1308964aed462e258f62ffe7a9cfce009
Erling K. Sæterdal <eks@Thexsoft.com>
2019-03-26 10:31:34 +0100

    Fixed missing flushing of memory stream to disk.

--- Source/tusdotnet/Stores/TusDiskStore.cs
+++ Source/tusdotnet/Stores/TusDiskStore.cs
@@ -109,6 +109,8 @@ namespace tusdotnet.Stores
                     bytesWritten += bytesRead;
 
                 } while (bytesRead != 0);
+                
+                file.Write(chunkBuffer.ToArray(), 0, (int)bytesWritten);
 
                 // Chunk is complete. Mark it as complete.
                 chunkComplete.Write("1");
