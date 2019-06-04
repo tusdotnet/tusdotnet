386eb793ef7494ade8080497ea92cc10de954781
Stefan Matsson <s.matsson@gmail.com>
2019-06-04 13:08:21 +0200

    Exclude coverage of xunit from code coverage

--- Tools/generate_code_coverage.ps1
+++ Tools/generate_code_coverage.ps1
@@ -3,6 +3,6 @@ If (Test-Path $output) {
 	Remove-Item $output -Force
 }
 
-dotnet test -f netcoreapp2.0 /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=$output ../source/tusdotnet.sln
+dotnet test -f netcoreapp2.0 /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=$output /p:Exclude="[xunit.*]*" ../source/tusdotnet.sln
 $key = Read-Host -Prompt 'Input Codecov key'
 ./codecov.sh -f "coverage.xml" -t $key
