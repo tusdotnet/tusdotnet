ff2520acf465fa42784e412e969fc578990b0e49
Stefan Matsson <s.matsson@gmail.com>
2019-09-25 11:56:46 +0200

    Add test site for ASP.NET Core 3.0 (.NET Core 3.0)

---
+++ Source/TestSites/AspNetCore_netcoreapp3.0_TestApp/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCore_netcoreapp3._0_TestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
