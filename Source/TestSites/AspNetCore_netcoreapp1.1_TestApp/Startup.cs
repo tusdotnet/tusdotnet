ec0547f9efc1d962e8d1e834b96e84946401203c
Stefan Matsson <s.matsson@gmail.com>
2020-02-05 14:08:49 +0100

    Update test sites to use AllowEmptyValues
    Also send metadata with an empty value to test the entire scenario

--- Source/TestSites/AspNetCore_netcoreapp1.1_TestApp/Startup.cs
+++ Source/TestSites/AspNetCore_netcoreapp1.1_TestApp/Startup.cs
@@ -64,6 +64,7 @@ namespace AspNetCore_netcoreapp1_1_TestApp
             {
                 UrlPath = "/files",
                 Store = new TusDiskStore(@"C:\tusfiles\"),
+                MetadataParsingStrategy = MetadataParsingStrategy.AllowEmptyValues,
                 Events = new Events
                 {
                     OnBeforeCreateAsync = ctx =>
@@ -75,12 +76,12 @@ namespace AspNetCore_netcoreapp1_1_TestApp
                             return Task.CompletedTask;
                         }
 
-                        if (!ctx.Metadata.ContainsKey("name"))
+                        if (!ctx.Metadata.ContainsKey("name") || ctx.Metadata["name"].HasEmptyValue)
                         {
                             ctx.FailRequest("name metadata must be specified. ");
                         }
 
-                        if (!ctx.Metadata.ContainsKey("contentType"))
+                        if (!ctx.Metadata.ContainsKey("contentType") || ctx.Metadata["contentType"].HasEmptyValue)
                         {
                             ctx.FailRequest("contentType metadata must be specified. ");
                         }
