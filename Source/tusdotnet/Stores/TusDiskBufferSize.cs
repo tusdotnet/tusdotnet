997aafff14294a8fed863f5220adc77e4525c1d5
Stefan Matsson <s.matsson@gmail.com>
2021-09-22 11:50:16 +0200

    Update comment that read buffer size is not used with PipeReader

--- Source/tusdotnet/Stores/TusDiskBufferSize.cs
+++ Source/tusdotnet/Stores/TusDiskBufferSize.cs
@@ -9,7 +9,7 @@ namespace tusdotnet.Stores
     {
         /// <summary>
         /// The default read buffer size used when nothing else is specified.
-        /// Default value is 50 KB.
+        /// Default value is 50 KB. 
         /// </summary>
         public const int DefaultReadBufferSizeInBytes = 51200;
 
@@ -36,7 +36,7 @@ namespace tusdotnet.Stores
         public int WriteBufferSizeInBytes { get; }
 
         /// <summary>
-        /// Initializes a new instance of the<see cref="TusDiskBufferSize"/> class using the provided write buffer size and <see cref="DefaultReadBufferSizeInBytes"/>.
+        /// Initializes a new instance of the <see cref="TusDiskBufferSize"/> class using the provided write buffer size and <see cref="DefaultReadBufferSizeInBytes"/>.
         /// </summary>
         /// <param name="writeBufferSizeInBytes">The amount of data to keep in memory before flushing data to disk. A higher value requires more RAM but uses less CPU and IO</param>
         public TusDiskBufferSize(int writeBufferSizeInBytes)
@@ -48,7 +48,7 @@ namespace tusdotnet.Stores
         /// Initializes a new instance of the <see cref="TusDiskBufferSize"/> class using the provided write and read buffer sizes.
         /// </summary>
         /// <param name="writeBufferSizeInBytes">The number of bytes to keep in memory before flushing data to disk. A higher value requires more RAM but uses less CPU and IO</param>
-        /// <param name="readBufferSizeInBytes">The number of bytes to read at the time from the request. The lower the value, the less data needs to be re-submitted on errors. However, the lower the value, the slower the operation is.</param>
+        /// <param name="readBufferSizeInBytes">The number of bytes to read at the time from the request. The lower the value, the less data needs to be re-submitted on errors. However, the lower the value, the slower the operation is. Please note that changing this value does nothing if using the request's PipeReader by setting <code>DefaultTusConfiguration.UsePipelinesIfAvailable = true</code> as it will always use the default PipeReader segment limit (4096 bytes)</param>
         public TusDiskBufferSize(int writeBufferSizeInBytes, int readBufferSizeInBytes)
         {
             WriteBufferSizeInBytes = AssertNonNegativeNumber(writeBufferSizeInBytes, nameof(writeBufferSizeInBytes));
@@ -57,7 +57,7 @@ namespace tusdotnet.Stores
 
         private int AssertNonNegativeNumber(int bufferSize, string nameOfParameter)
         {
-            if(bufferSize <= 0)
+            if (bufferSize <= 0)
             {
                 throw new TusConfigurationException($"{nameOfParameter} is zero or a negative number");
             }
