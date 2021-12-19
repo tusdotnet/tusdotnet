b37993ccb8eea89f4722d27b9a6180cd040cb21c
Stefan Matsson <s.matsson@gmail.com>
2021-12-19 10:12:10 +0100

    Support dissallowing cancellation in options

--- Source/tusdotnet/Tus2/Tus2Options.cs
+++ Source/tusdotnet/Tus2/Tus2Options.cs
@@ -9,5 +9,7 @@ namespace tusdotnet.Tus2
     public class Tus2Options
     {
         public string FolderDiskPath { get; set; }
+
+        public bool AllowClientToDeleteFile { get; set; }
     }
 }
