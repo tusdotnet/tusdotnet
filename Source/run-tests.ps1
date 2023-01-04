1c4c5186571c4e1083a808320e4cd5501ac84550
Stefan Matsson <s.matsson@gmail.com>
2023-01-04 15:07:50 +0100

    Make tests work in later versions of VS/dotnet
    * Exlude old runtimes as newer VS does not handle them very well
    * Run all tests from run-tests.ps1 and run them on a supported SDK
    * Update nugets

--- Source/run-tests.ps1
+++ Source/run-tests.ps1
@@ -1,3 +1,31 @@
+
+$net6sdk = "6.0.404";
+$net7sdk = "7.0.101"
+$runtimesOnNet6 = "net452", "netcoreapp2.1", "netcoreapp2.2", "netcoreapp3.0", "netcoreapp3.1", "net6.0";
+$runtimesOnNet7 = "net7.0";
+
 Push-Location tusdotnet.test
-dotnet test -c Release -v q --nologo
+
+Function RunTests([string]$sdkVersion, [string[]]$runtimes)
+{
+    Write-Host "Going to run tests for ""$runtimes"" using SDK $sdkVersion"
+
+    dotnet new globaljson --sdk-version $sdkVersion --force | Out-Null
+
+    Foreach ($item in $runtimes) {
+        dotnet test -c Release -v q --nologo --no-build -f $item
+    }
+
+   Remove-Item -Force .\global.json | Out-Null
+}
+
+# Build all once as this will still build for all runtimes. Ignore warning that some runtimes are EOL.
+dotnet build -c release -v q -nowarn:NETSDK1138
+
+# We need to run this on .NET 6.x as the .NET7 SDK does not work with running older netcoreapp (<3.0): Unhandled Exception: System.IO.FileLoadException: Could not load file or assembly 'System.Runtime, Version=4.2.2.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'. The located assembly's manifest definition does not match the assembly reference. (Exception from HRESULT: 0x80131040)
+RunTests $net6sdk $runtimesOnNet6;
+
+# Run the rest on .NET7
+RunTests $net7sdk $runtimesOnNet7;
+
 Pop-Location
