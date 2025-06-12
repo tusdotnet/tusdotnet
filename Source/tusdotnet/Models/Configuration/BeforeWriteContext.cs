7de5208c2c18497c006201b37d271b93ae434ccd
Stefan Matsson <s.matsson@gmail.com>
2025-06-12 15:50:52 +0200

    Update xmldoc again

--- Source/tusdotnet/Models/Configuration/BeforeWriteContext.cs
+++ Source/tusdotnet/Models/Configuration/BeforeWriteContext.cs
@@ -6,7 +6,7 @@
 public class BeforeWriteContext : ValidationContext<BeforeWriteContext>
 {
     /// <summary>
-    /// The current upload offset of the file
+    /// The current upload offset of the file.
     /// </summary>
     public long UploadOffset { get; set; }
 }
