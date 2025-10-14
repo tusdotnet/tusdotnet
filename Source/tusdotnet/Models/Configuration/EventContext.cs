dec8644201c637f0081981191238be8b8936a41f
Stefan Matsson <s.matsson@gmail.com>
2025-10-14 14:49:17 +0200

    Minor performance optimizations in EventContext

--- Source/tusdotnet/Models/Configuration/EventContext.cs
+++ Source/tusdotnet/Models/Configuration/EventContext.cs
@@ -17,6 +17,11 @@ namespace tusdotnet.Models.Configuration
     public abstract class EventContext<TSelf>
         where TSelf : EventContext<TSelf>, new()
     {
+        // Cache completed null task to avoid allocation on every null FileId call
+        private static readonly Task<ITusFile> s_completedNullFileTask = Task.FromResult<ITusFile>(
+            null
+        );
+
         /// <summary>
         /// The id of the file that was completed
         /// </summary>
@@ -53,18 +58,14 @@ namespace tusdotnet.Models.Configuration
         public Task<ITusFile> GetFileAsync()
         {
             if (string.IsNullOrEmpty(FileId))
-                return Task.FromResult<ITusFile>(null);
+                return s_completedNullFileTask;
 
             return ((ITusReadableStore)Store).GetFileAsync(FileId, CancellationToken);
         }
 
         internal static TSelf Create(ContextAdapter context, Action<TSelf> configure = null)
         {
-            var fileId = context.FileId;
-            if (string.IsNullOrEmpty(fileId))
-            {
-                fileId = null;
-            }
+            var fileId = string.IsNullOrEmpty(context.FileId) ? null : context.FileId;
 
             var eventContext = new TSelf
             {
